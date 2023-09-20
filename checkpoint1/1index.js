4222e5f0dc08dfc017522b7551cff39908ba1f31
Marta <marta.ferraris25@gmail.com>
2023-09-20 11:30:42 +0200

    checkpoint

---
+++ checkpoint1/1index.js
@@ -0,0 +1,11 @@
+const words = ['spray', 'limit', 'elite', 'exuberant', 'present'];
+
+// function uppercaseWord(arr) {
+    
+// }
+
+words.forEach((word)=> {
+    word.toUpperCase();
+    
+    console.log(word.toUpperCase());
+})
